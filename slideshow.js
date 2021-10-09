09069760c7577da6df8fade74dd533c4c611d972
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-10-09 14:35:42 +0200

    js added

--- slideshow.js
+++ slideshow.js
@@ -8,60 +8,49 @@ async function fetchData(URL) {
 
 async function getData(URL) {
   const data = await fetchData(URL);
-  displaySlideShow(data);
+  displaySlideShow(data, 0);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   getData('./data.json');
 });
 
-function displaySlideShow(data) {
-  let dataHtml = data
-    .map((item) => {
-      let { images, name, artist, year, description, source } = item;
-
-      return `
-  <div class="single-slideshow">
-    <div class="img-container">
-      <img src="${images.hero.small}" alt="" class="slideshow-main-img" />
-      <div class="view-image">
-        <img src="./assets/shared/icon-view-image.svg" alt="" />
-        <p>view image</p>
+function displaySlideShow(data, start) {
+  let { images, name, artist, year, description, source } = data[start];
+  let slideHml = `
+  <div class="gallery-image">
+  <div>
+    <p class="close-btn">close</p>
+    <img src="${images.gallery}" alt="${name}" />
+  </div>
+  </div>
+    <div class="single-slideshow">
+      <div class="img-container">
+        <img src="${images.hero.small}" alt="" class="slideshow-main-img" />
+        <div class="view-image">
+          <img src="./assets/shared/icon-view-image.svg" alt="" />
+          <p>view image</p>
+        </div>
+        <div class="slideshow-header">
+          <h1 class="head1">${name}</h1>
+          <p class="shead1">${artist.name}</p>
+        </div>
+        <img src="${artist.image}" alt="" class="artist-img" />
       </div>
-      <div class="slideshow-header">
-        <h1 class="head1">${name}</h1>
-        <p class="shead1">${artist.name}</p>
-      </div>
-      <img src="${artist.image}" alt="" class="artist-img" />
-    </div>
-    <div class="painting-info">
-      <p>${description}</p>
-      <a href="${source}" class="link">
-        go to source
-      </a>
-      <span class="date">${year}</span>
-    </div>
-  </div> `;
-    })
-    .join(' ');
+      <div class="painting-info">
+        <p>${description}</p>
+        <a href="${source}" class="link">
+          go to source
+        </a>
+        <span class="date">${year}</span>
+      </div>`;
 
-  slideMain.innerHTML = dataHtml;
+  slideMain.innerHTML = slideHml;
 
-  const slides = document.querySelectorAll('.single-slideshow');
+  const viewImage = document.querySelector('.view-image');
+  const galleryImg = document.querySelector('.gallery-image');
+  const closeBtn = document.querySelector('.close-btn');
 
-  slides.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`;
-  });
-
-  let counter = 1;
-  slides.forEach((slide) => {
-    slide.style.transform = `translateX(-${counter * 100}%)`;
-  });
+  viewImage.addEventListener('click', () => galleryImg.classList.add('show'));
+  closeBtn.addEventListener('click', () => galleryImg.classList.remove('show'));
 }
-
-// <div class="gallery-image">
-//   <div>
-//     <p class="close">close</p>
-//     <img src="${images.gallery}" alt="${name}" />
-//   </div>
-// </div>
